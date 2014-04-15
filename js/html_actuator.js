4d18499068ac7744f447ce7e43ac8e9e7d254985
dolan2048 <dolan2048@gmail.com>
2014-04-16 00:32:24 +0200

    update

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -3,7 +3,6 @@ function HTMLActuator() {
   this.scoreContainer   = document.querySelector(".score-container");
   this.bestContainer    = document.querySelector(".best-container");
   this.messageContainer = document.querySelector(".game-message");
-  this.sharingContainer = document.querySelector(".score-sharing");
 
   this.score = 0;
 }
@@ -37,11 +36,7 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 };
 
 // Continues the game (both restart and keep playing)
-HTMLActuator.prototype.continue = function () {
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "restart");
-  }
-
+HTMLActuator.prototype.continueGame = function () {
   this.clearMessage();
 };
 
@@ -131,38 +126,14 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "Jej, pobjeda!" : "Kraj igre!";
-
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
-  }
+  var message = won ? "You win!" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
-
-  this.clearContainer(this.sharingContainer);
-  this.sharingContainer.appendChild(this.scoreTweetButton());
-  twttr.widgets.load();
 };
 
 HTMLActuator.prototype.clearMessage = function () {
   // IE only takes one value to remove at a time.
   this.messageContainer.classList.remove("game-won");
   this.messageContainer.classList.remove("game-over");
-};
-
-HTMLActuator.prototype.scoreTweetButton = function () {
-  var tweet = document.createElement("a");
-  tweet.classList.add("twitter-share-button");
-  tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "spojipoliticare");
-  tweet.setAttribute("data-url", "http://git.io/djKgDQ");
-  tweet.setAttribute("data-counturl", "http://spojipoliticare.github.io/igra/");
-  tweet.textContent = "Tweet";
-
-  var text = "Gle, imam " + this.score + " bodova u igri spoji politicare, igra u kojoj " +
-             "spajas politicare! #spojipoliticare";
-  tweet.setAttribute("data-text", text);
-
-  return tweet;
 };
