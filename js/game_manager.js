251dbe70212aa08e06a8763d9fd2ca12d28cc6e0
dolan2048 <dolan2048@gmail.com>
2014-04-16 09:14:27 +0200

    update

--- js/game_manager.js
+++ js/game_manager.js
@@ -168,7 +168,7 @@ GameManager.prototype.move = function (direction) {
           tile.updatePosition(positions.next);
 
           // Update the score
-          self.score += merged.value;
+          self.score -= merged.value;
 
           // The mighty 2048 tile
           if (merged.value === 2048) self.won = true;
