ee353acf491364d300b19c0b694db06dd0d04592
dolan2048 <dolan2048@gmail.com>
2014-04-16 10:40:30 +0200

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -81,7 +81,7 @@ GameManager.prototype.addRandomTile = function () {
 
 // Sends the updated grid to the actuator
 GameManager.prototype.actuate = function () {
-  if (this.storageManager.getBestScore() < this.score) {
+  if (this.storageManager.getBestScore() > this.score) {
     this.storageManager.setBestScore(this.score);
   }
 
